18965ceb44be8862758dd107969f3c3cf58d9322
Hardaat Singh Baath <hardaatbaath11@gmail.com>
2025-01-11 19:10:25 +0530

    Create siglip.py

---
+++ siglip.py
@@ -0,0 +1,92 @@
+from typing import Optional, Tuple
+import torch
+import torch.nn as nn
+
+class SiglipVisionConfig:
+
+    def __init__(
+        self,
+        hidden_size: int = 768, # Hidden intermediate encoder size
+        intermediate_size: int = 3072, # Size of the linear layer
+        num_hidden_layers: int = 12, # Number of hidden layers of Vision transformer
+        num_attention_heads: int = 12, # Number of attention heads
+        num_channels: int = 3, # Number of image channels
+        image_size: int = 224, # Image size
+        patch_size: int = 16, # Patch size (of the image)
+        layer_norm_eps = 1e-6, # Layer norm epsilon
+        attention_dropout: float = 0.0, # Attention dropout
+        num_image_tokens: int = None, # Number of image tokens
+        **kwargs
+    ):
+        super().__init__()
+
+        self.hidden_size = hidden_size
+        self.intermediate_size = intermediate_size
+        self.num_hidden_layers = num_hidden_layers
+        self.num_attention_heads = num_attention_heads
+        self.num_channels = num_channels
+        self.patch_size = patch_size
+        self.image_size = image_size
+        self.attention_dropout = attention_dropout
+        self.layer_norm_eps = layer_norm_eps
+        self.num_image_tokens = num_image_tokens
+
+class SiglipVisionEmbeddings(nn.Module):
+    def __init__(self, config: SiglipVisionConfig):
+        super().__init__()
+        self.config = config
+        self.embed_dim = config.hidden_size
+        self.image_size = config.image_size
+        self.patch_size = config.patch_size
+
+        self.patch_embedding = nn.Conv2d(
+            in_channels=config.num_channels,
+            out_channels=self.embed_dim,
+            kernel_size=self.patch_size, # If the patch size = kernel size, then we get a single patch
+            stride=self.patch_size, #  stride = patch size, then we get non-overlapping patches
+            padding="valid", # This indicates no padding is added
+        )
+
+        self.num_patches = (self.image_size // self.patch_size) ** 2
+        self.num_positions = self.num_patches
+        self.position_embedding = nn.Embedding(self.num_positions, self.embed_dim)
+        self.register_buffer(
+            "position_ids",
+            torch.arange(self.num_positions).expand((1, -1)),
+            persistent=False,
+        )
+
+
+class SiglipVisionTransformer(nn.Module):
+
+    def __init__(self, config: SiglipVisionConfig):
+        super().__init__()
+        self.config = config
+        embed_dim = config.hidden_size
+
+        self.embeddings = SiglipVisionEmbeddings(config)
+        self.encoder = SiglipVisionEncoder(config)
+        self.post_layer_norm = nn.LayerNorm(embed_dim, eps=config.layer_norm_eps)
+
+    def forward(self, pixel_values: torch.Tensor) -> torch.Tensor :
+        # pixel_values: [Batch_Size, Channels, Height, Width] -> [Batch_Size, Num_Patches, Embed_Dim]
+        hidden_states = self.embeddings(pixel_values)
+
+        last_hidden_state = self.encoder(inputs_embeds=hidden_states)
+
+        last_hidden_state = self.post_layernorm(last_hidden_state)
+
+        return last_hidden_state
+
+
+class SiglipVisionModel(nn.Module):
+    """ Vision Transformer with a simple classifier head. Takes an image and then returns the embeddings for each patch """
+    def __init__(self, config: SiglipVisionConfig):
+        super().__init__()
+        self.config = config
+        self.vision_model = SiglipVisionTransformer(config)
+
+    def forward(self, pixel_values) -> Tuple:
+        # [Batch_Size, Channels, Height, Width] -> [Batch_Size, Num_Patches, Embed_Dim]
+        return self.vision_model(pixel_values=pixel_values) 
+
