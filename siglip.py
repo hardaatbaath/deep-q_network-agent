c2c073b871084c14899c9e26bd20503044cd8577
Hardaat Singh Baath <hardaatbaath11@gmail.com>
2025-01-12 12:35:04 +0530

    Update siglip.py

--- siglip.py
+++ siglip.py
@@ -47,14 +47,31 @@ class SiglipVisionEmbeddings(nn.Module):
             padding="valid", # This indicates no padding is added
         )
 
-        self.num_patches = (self.image_size // self.patch_size) ** 2
-        self.num_positions = self.num_patches
-        self.position_embedding = nn.Embedding(self.num_positions, self.embed_dim)
+        self.num_patches = (self.image_size // self.patch_size) ** 2 # Number of Patches ^ 2
+        self.num_positions = self.num_patches # Num of positions  = num of patches
+        self.position_embedding = nn.Embedding(self.num_positions, self.embed_dim) # generates num_pos embeddings of size embed_dim
+        
         self.register_buffer(
             "position_ids",
             torch.arange(self.num_positions).expand((1, -1)),
             persistent=False,
         )
+
+    def forward(self, pixel_values: torch.FloatTensor) -> torch.Tensor:
+        _, _, height, width = pixel_values.shape # [Batch_Size, Channels, Height, Width]
+        # Convolve the `patch_size` kernel over the image, with no overlapping patches since the stride is equal to the kernel size
+        # The output of the convolution will have shape [Batch_Size, Embed_Dim, Num_Patches_H, Num_Patches_W]
+        # where Num_Patches_H = height // patch_size and Num_Patches_W = width // patch_size
+        patch_embeds = self.patch_embedding(pixel_values)  
+        # [Batch_Size, Embed_Dim, Num_Patches_H, Num_Patches_W] -> [Batch_Size, Embed_Dim, Num_Patches]
+        # where Num_Patches = Num_Patches_H * Num_Patches_W
+        embeddings = patch_embeds.flatten(2)
+        # [Batch_Size, Embed_Dim, Num_Patches] -> [Batch_Size, Num_Patches, Embed_Dim]
+        embeddings = embeddings.transpose(1, 2)
+        # Add position embeddings to each patch. Each positional encoding is a vector of size [Embed_Dim]
+        embeddings = embeddings + self.position_embedding(self.position_ids)
+        # [Batch_Size, Num_Patches, Embed_Dim]
+        return embeddings
 
 
 class SiglipVisionTransformer(nn.Module):
